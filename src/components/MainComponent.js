281f78ed65c56c21f2ec750420088c1c47777a6b
Andrius Pratusis <andrius.pratusis1993@gmail.com>
2022-10-19 22:50:42 +0300

    updates

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,9 @@ class MainComponent extends Component {
     super(props);
     this.settings = {
       clientId: `client_L6G18CbSFJtGWbtz2xsuZ3`,
+      grammarLink: `https://github.com/Sparky4567/grammarly`,
+      message: `Source code is public now (change your client id and spin off your
+        own versions) [I recommend Github and Cloudflare pages to make development faster]`,
     };
     this.textAreaStyle = {
       height: 80 + `vh`,
@@ -19,6 +22,20 @@ class MainComponent extends Component {
     return (
       <div className="container">
         <div className="row align-items-center vh-100">
+          <div className="col-12 text-center">
+            <div className="card">
+              <p>{this.settings.message}</p>
+              <p>
+                <a
+                  href={this.settings.grammarLink}
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  {this.settings.grammarLink}
+                </a>
+              </p>
+            </div>
+          </div>
           <div className="col-12">
             <GrammarlyEditorPlugin clientId={this.settings.clientId}>
               <textarea className="form-control" style={this.textAreaStyle} />
